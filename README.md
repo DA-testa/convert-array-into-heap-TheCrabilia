687caff0514a74dc9e54074b9fd49166389d96f3
Ilja Spirts <69972734+TheCrabilia@users.noreply.github.com>
2023-03-18 21:01:21 +0000

    Add solution

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 [![Open in Codespaces](https://classroom.github.com/assets/launch-codespace-f4981d0f882b2a3f0472912d15f9806d57e124e0fc890972558857b51b24a6f9.svg)](https://classroom.github.com/open-in-codespaces?assignment_repo_id=10543162)
+
+## Ilja Spirts 221RDB074
+
 #  Problem: Convert array into heap
+
 In this problem you will convert an array of integers into a heap. This is the crucial step of the sorting
 algorithm called HeapSort. It has guaranteed worst-case running time of 𝑂(𝑛 log 𝑛) as opposed to QuickSort’s
 average running time of 𝑂(𝑛 log 𝑛). QuickSort is usually used in practice, because typically it is faster, but
@@ -7,7 +11,9 @@ HeapSort is used for external sort when you need to sort huge files that don’t
 computer.
 
 # Problem description
-## Task. 
+
+## Task.
+
 The first step of the HeapSort algorithm is to create a heap from the array you want to sort. By the
 way, did you know that algorithms based on Heaps are widely used for external sort, when you need
 to sort huge files that don’t fit into memory of a computer?
@@ -16,13 +22,18 @@ do that by applying a certain number of swaps to the array. Swap is an operation
 elements 𝑎𝑖 and 𝑎𝑗 of the array 𝑎 for some 𝑖 and 𝑗. You will need to convert the array into a heap using
 only 𝑂(𝑛) swaps, as was described in the lectures. Note that you will need to use a min-heap instead
 of a max-heap in this problem.
-## Input Format. 
+
+## Input Format.
+
 The first line of the input contains single integer 𝑛. The next line contains 𝑛 space-separated
 integers 𝑎𝑖.
-## Constraints. 
+
+## Constraints.
+
 1 ≤ 𝑛 ≤ 100 000; 0 ≤ 𝑖, 𝑗 ≤ 𝑛 − 1; 0 ≤ 𝑎0, 𝑎1, . . . , 𝑎𝑛−1 ≤ 109. All 𝑎𝑖 are distinct.
 
-## Output Format. 
+## Output Format.
+
 The first line of the output should contain single integer 𝑚 — the total number of swaps.
 𝑚 must satisfy conditions 0 ≤ 𝑚 ≤ 4𝑛. The next 𝑚 lines should contain the swap operations used
 to convert the array 𝑎 into a heap. Each swap is described by a pair of integers 𝑖, 𝑗 — the 0-based
@@ -30,11 +41,12 @@ indices of the elements to be swapped. After applying all the swaps in the speci
 must become a heap, that is, for each 𝑖 where 0 ≤ 𝑖 ≤ 𝑛 − 1 the following conditions must be true:
 1. If 2𝑖 + 1 ≤ 𝑛 − 1, then 𝑎𝑖 < 𝑎2𝑖+1.
 2. If 2𝑖 + 2 ≤ 𝑛 − 1, then 𝑎𝑖 < 𝑎2𝑖+2.
-3. 
+3.
 Note that all the elements of the input array are distinct. Note that any sequence of swaps that has
 length at most 4𝑛 and after which your initial array becomes a correct heap will be graded as correct.
 
-## Sample 
+## Sample
+
 Input:
 5
 
--- build_heap.py
+++ build_heap.py
@@ -1,38 +1,62 @@
-# python3
+import os
 
 
-def build_heap(data):
+def heapify_down(arr: list[int], i: int, swaps: tuple[int, int]) -> int:
+    left_child = 2 * i + 1
+    right_child = 2 * i + 2
+    smallest = i
+
+    if left_child < len(arr) and arr[left_child] < arr[smallest]:
+        smallest = left_child
+
+    if right_child < len(arr) and arr[right_child] < arr[smallest]:
+        smallest = right_child
+
+    if smallest != i:
+        arr[i], arr[smallest] = arr[smallest], arr[i]
+        swaps.append((i, smallest))
+        swaps = heapify_down(arr, smallest, swaps)
+
+    return swaps
+
+
+def build_heap(data: list[int]) -> int:
+    middle = len(data) // 2
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
 
+    # Iterate from the middle of the array to the beginning
+    for i in range(middle, -1, -1):
+        swaps = heapify_down(data, i, swaps)
+
+    # Iterate from the middle of the array to the end
+    for i in range(middle + 1, len(data)):
+        swaps = heapify_down(data, i, swaps)
 
     return swaps
 
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
+    mode = input("Enter mode (I - manual, F - read from file): ")
+    match mode:
+        case "I":
+            n = int(input("Enter number of elements: "))
+            data = list(map(int, input("Enter elements: ").split()))
+            assert len(data) == n
+            swaps = build_heap(data)
+        case "F":
+            file_name = input("Enter test file name: ")
+            if file_name.endswith(".a"):
+                print("File name should not end with '.a'")
+                os.exit(1)
+            with open(f"tests/{file_name}", "r") as f:
+                n = int(f.readline())
+                data = list(map(int, f.readline().split()))
+                assert len(data) == n
+                swaps = build_heap(data)
+        case _:
+            print("Invalid mode")
+            os.exit(1)
 
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
